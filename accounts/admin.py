4f8c42dd62be541ff705b6cb741b9cc200edfe17
John_6670 <mayn2005m@gmail.com>
2025-02-05 22:41:59 +0330

    account admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,12 +8,45 @@ class AddressInline(admin.TabularInline):
     extra = 1
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['full_name', 'email', 'phone_number', 'date_of_birth', 'is_staff']
-    list_filter = ['is_staff', 'date_of_birth']
-    search_fields = ['username', 'email', 'full_name']
-    inlines = [AddressInline]
+    inlines = (AddressInline,)
+    list_display = ('full_name', 'email', 'phone_number', 'date_of_birth', 'national_id')
+    list_display_links = ('full_name', 'email')
+    search_fields = ('full_name', 'email', 'phone_number', 'national_id')
+    list_filter = ('date_of_birth', 'is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'phone_number')}),
+        ('Personal info', {
+            'classes': ('collapse',),
+            'fields': ('first_name', 'last_name', 'national_id', 'date_of_birth')
+        }),
+        ('Permissions', {
+            'classes': ('collapse',),
+            'fields': ('is_active', 'is_staff', 'is_superuser')
+        }),
+        ('Important dates', {
+            'classes': ('collapse',),
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined', 'last_login')
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Address)
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'country', 'city', 'street_address', 'postal_code', 'default')
+    list_display_links = ('user', 'country')
+    search_fields = ('user__username', 'user__email', 'country', 'city', 'street_address', 'postal_code')
+    list_filter = ('country', 'city', 'default')
+    fieldsets = (
+        (None, {'fields': ('user', 'country', 'city', 'street_address')}),
+        ('Advanced Fields', {'fields': ('apartment_address', 'postal_code')}),
+        ('Default', {
+            'classes': ('collapse',),
+            'fields': ('default',)
+        }),
+    )
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
