eb68e32db9b173f2485c5183c2f2f184a13530a0
John_6670 <mayn2005m@gmail.com>
2025-02-05 22:51:12 +0330

    product admin

--- products/admin.py
+++ products/admin.py
@@ -1,2 +1,32 @@
 from django.contrib import admin
 
+from .models import ProductCategory, Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'parent')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('parent',)
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'stock', 'category', 'discount')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('category', 'stock')
+    search_fields = ('name', 'desc', 'category__name', 'slug')
+    inlines = [ProductImageInline]
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'image')
+    search_fields = ('product__name', 'product__slug')
+    list_filter = ('product',)
