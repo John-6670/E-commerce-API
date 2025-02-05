d480a6a814270d31875beab354319921808b93bf
John_6670 <mayn2005m@gmail.com>
2025-02-05 21:56:46 +0330

    merged with models

--- products/admin.py
+++ products/admin.py
@@ -1,20 +1,2 @@
 from django.contrib import admin
 
-from .models import ProductCategory, Product, ProductImage
-
-
-class ProductImageInline(admin.TabularInline):
-    model = ProductImage
-    extra = 1
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'quantity', 'created_at', 'updated_at']
-    list_filter = ['category']
-    search_fields = ['name', 'desc']
-    inlines = [ProductImageInline]
-
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductCategory)
-admin.site.register(ProductImage)
