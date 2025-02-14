9ec14130605e1f5aeca284bcf9932d8574200374
John_6670 <mayn2005m@gmail.com>
2025-02-14 14:55:23 +0330

    fixed signal issue

--- orders/signals.py
+++ orders/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_migrate
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 
@@ -7,7 +7,7 @@ from .models import Cart, Order
 User = get_user_model()
 
 
-@receiver(post_save, sender=User)
+@receiver(post_migrate, sender=User)
 def create_cart(sender, instance, created, **kwargs):
-    if created:
+    if created and not instance.is_superuser:
         Cart.objects.create(user=instance)
